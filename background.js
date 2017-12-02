c4016b6d639a828e996969f3d9a59e27e618b9ac
kuwa72 <ykuwashima@gmail.com>
2017-12-03 05:35:32 +0900

    Revice reapplicate by icon click

--- background.js
+++ background.js
@@ -1,10 +1,12 @@
-/*chrome.browserAction.onClicked.addListener(function(tab) {
+chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript(null, {file: "insert-search-function.js"});
-});*/
+});
+/*
 function handleClick() {
   browser.runtime.openOptionsPage();
 }
 browser.browserAction.onClicked.addListener(handleClick);
+*/
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
   if (changeInfo.status == 'complete') {
     var gettingItem = browser.storage.local.get('urls');
