20179630c0194a31d267ccf10bf5cc03bc232b9f
kuwa72 <ykuwashima@gmail.com>
2017-12-03 17:02:24 +0900

    Fix option dialog

--- background.js
+++ background.js
@@ -9,15 +9,12 @@ browser.browserAction.onClicked.addListener(handleClick);
 */
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
   if (changeInfo.status == 'complete') {
-    var gettingItem = browser.storage.local.get('urls');
-    gettingItem.then((res) => {
-      if (res.urls
-        .split("\n")
-        .filter(x => x != '')
-        .map(x => new RegExp(x))
-        .some(x => tab.url.match(x))) {
-          chrome.tabs.executeScript(null, {file: "insert-search-function.js"});
-      }
-    });
+    var urls = localStorage.getItem("urls");
+    if (urls.split("\n")
+      .filter(x => x != '')
+      .map(x => new RegExp(x))
+      .some(x => tab.url.match(x))) {
+      chrome.tabs.executeScript(null, {file: "insert-search-function.js"});
+    }
   }
 });
--- options.js
+++ options.js
@@ -1,15 +1,10 @@
 function saveOptions(e) {
-  browser.storage.local.set({
-    urls: document.querySelector("#urls").value
-  });
+  localStorage.setItem("urls", document.querySelector("#urls").value);
   e.preventDefault();
 }
 
 function restoreOptions() {
-  var gettingItem = browser.storage.local.get('urls');
-  gettingItem.then((res) => {
-    document.querySelector("#urls").value = res.urls || ''
-  });
+    document.querySelector("#urls").value = localStorage.getItem("urls");
 }
 
 document.addEventListener('DOMContentLoaded', restoreOptions);
